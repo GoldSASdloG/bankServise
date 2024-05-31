b583f596a9b8d92efa5d6314e01327a9cedee0d5
GoldSASdloG <Procatulus@gmail.com>
2024-05-31 14:49:16 +0300

    validateEmail validatePhoneNumber

--- src/main/java/org/example/BankService.java
+++ src/main/java/org/example/BankService.java
@@ -1,4 +1,21 @@
 package org.example;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class BankService {
+    public static boolean validateEmail(String email) {
+        String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(email);
+
+        return matcher.matches();
+    }
+    public static boolean validatePhoneNumber(String phoneNumber) {
+        String phoneRegex = "^\\+7 \\(\\d{3}\\) \\d{3}-\\d{2}-\\d{2}$";
+        Pattern pattern = Pattern.compile(phoneRegex);
+        Matcher matcher = pattern.matcher(phoneNumber);
+
+        return matcher.matches();
+    }
 }
